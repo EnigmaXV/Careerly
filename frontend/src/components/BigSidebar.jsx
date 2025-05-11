291b12d7e3a7a35b83d36558e3982102f3bf1cf0
Omar Awad <omarawad@Mac.fritz.box>
2025-05-11 23:45:56 +0200

    feat: add Dashboard layout with Navbar and Sidebar components

---
+++ frontend/src/components/BigSidebar.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router";
+import { FiPlusCircle, FiList, FiBarChart2, FiUser } from "react-icons/fi";
+import { LuLogOut } from "react-icons/lu";
+import { useDashboardContext } from "../pages/DashboardLayout";
+
+const BigSidebar = () => {
+  const { user, setIsSidebarOpen } = useDashboardContext();
+  return (
+    <Wrapper onClick={() => setIsSidebarOpen(false)}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          justifyContent: "space-between",
+        }}
+      >
+        <Nav>
+          <StyledLink to="/add-job">
+            <FiPlusCircle /> Add Job
+          </StyledLink>
+          <StyledLink to="/all-jobs">
+            <FiList /> All Jobs
+          </StyledLink>
+          <StyledLink to="/stats">
+            <FiBarChart2 /> Stats
+          </StyledLink>
+          <StyledLink to="/profile">
+            <FiUser /> Profile
+          </StyledLink>
+        </Nav>
+
+        <Logout>
+          <LuLogOut /> Logout
+        </Logout>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: none;
+  @media (max-width: 992px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+    background-color: #000000;
+    box-shadow: var(--shadow-4);
+    margin-top: 20px;
+    height: 100vh;
+    padding: 1rem 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  font-size: 0.95rem;
+  color: #f1f3f5;
+  text-decoration: none;
+  transition: color 0.2s ease, transform 0.2s ease;
+
+  &:hover {
+    color: #00a8e8;
+    transform: translateX(3px);
+  }
+
+  & svg {
+    font-size: 1.2rem;
+  }
+`;
+
+const Logout = styled.div`
+  gap: 0.75rem;
+  font-size: 0.95rem;
+  color: #f1f3f5;
+  text-decoration: none;
+  transition: color 0.2s ease;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    color: #e63946;
+    cursor: pointer;
+  }
+
+  & svg {
+    font-size: 1.2rem;
+  }
+`;
+
+export default BigSidebar;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import Logo from "./Logo";
+import Switch from "./Switch";
+import styled from "styled-components";
+import img from "../assets/images/avatar-2.jpg";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import Burger from "./Burger";
 
 const Navbar = () => {
-  return <div></div>;
+  const { user, isSidebarOpen } = useDashboardContext();
+  return (
+    <StyledWrapper>
+      <Burger />
+      <Logo />
+      <div className="user">
+        <Switch />
+        <div className="user__img">
+          <img src={img} alt="user" />
+        </div>
+        <div className="user__info">
+          <span>Omar</span>
+        </div>
+      </div>
+    </StyledWrapper>
+  );
 };
 
+const StyledWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  box-shadow: var(--shadow-4);
+  position: relative;
+  background-color: #000000;
+  height: var(--nav-height);
+  .user {
+    display: flex;
+    align-items: center;
+
+    &__img {
+      margin-left: 1rem;
+      img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        object-fit: cover;
+      }
+    }
+
+    &__info {
+      margin-left: 1rem;
+      span {
+        font-weight: bold;
+      }
+    }
+  }
+`;
+const NavbarStyled = styled(Navbar)`
+  ${StyledWrapper}
+`;
 export default Navbar;
