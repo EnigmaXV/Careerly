fc6034d6534d3a245a61e89b33c493685e431c0d
Omar Awad <omarawad@Mac.fritz.box>
2025-06-06 01:40:41 +0200

    feat: enhance user statistics retrieval and add dayjs for date formatting

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,6 +12,7 @@ const upload = require("../middlewares/uploadMiddleware");
 
 router.route("/me").get(protect, getCurrentUser);
 router.route("/me").patch(protect, upload.single("profileImage"), updateUser);
+router.route("/stats").get(protect, getStats);
 router.route("/admin-stats").get(protect, restrictTo("admin"), getAdminStats);
 
 module.exports = router;
